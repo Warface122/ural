7fcf01098db953c6aae987aae590f8fcc94f60a1
Warface122 <fwwqeqweqfsaffaf@gmail.com>
2025-03-19 18:37:35 +0400

    Update Step 2: install_vim.sh

--- Step 2: install_vim.sh
+++ Step 2: install_vim.sh
@@ -14,6 +14,26 @@ make
 make install
 
 echo "Обновляем PATH..."
+# Файл, в который нужно добавить команду
+TARGET_FILE="$HOME/.bashrc.user"
+
+# Команда, которую нужно добавить
 export PATH=$HOME/vim-install/bin:$PATH
+
+# Проверка, существует ли файл
+if [ ! -f "$TARGET_FILE" ]; then
+    # Если файл не существует, создаем его
+    touch "$TARGET_FILE"
+fi
+
+# Проверка, есть ли команда уже в файле
+if ! grep -Fxq "$COMMAND" "$TARGET_FILE"; then
+    # Если команды нет, добавляем её
+    echo "$COMMAND" >> "$TARGET_FILE"
+    echo "Команда добавлена в $TARGET_FILE"
+else
+    echo "Команда уже существует в $TARGET_FILE"
+fi
+
 
 echo "Установка завершена!"
